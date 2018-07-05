91174b24eaa26c288cb18453d27f120673256392
Nate Iler <nate@flipboxdigital.com>
2018-07-05 11:57:05 -0600

    docblocks

--- src/helpers/TransformerHelper.php
+++ src/helpers/TransformerHelper.php
@@ -58,7 +58,7 @@ class TransformerHelper extends \Flipbox\Transform\Helpers\TransformerHelper
 
     /**
      * @param TransformerCollectionInterface|array|string|null $transformer
-     * @param string|null $default
+     * @param array|callable|TransformerCollectionInterface|null $default
      * @return TransformerCollectionInterface|null
      */
     public static function resolveCollection($transformer = null, $default = ['class' => TransformerCollection::class])
