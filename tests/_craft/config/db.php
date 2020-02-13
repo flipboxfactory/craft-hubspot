147350db3e8fbbe1bcfdd67c443b0ebe5288bf98
Nate Iler <nate@flipboxdigital.com>
2020-02-13 10:43:53 -0700

    Updating to DSN format (Craft 3.4)

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -9,13 +9,10 @@
  */
 
 return [
-    'driver' => getenv('DB_DRIVER'),
-    'server' => getenv('DB_SERVER'),
+    'dsn' => getenv('DB_DSN'),
     'user' => getenv('DB_USER'),
     'password' => getenv('DB_PASSWORD'),
-    'database' => getenv('DB_DATABASE'),
     'schema' => getenv('DB_SCHEMA'),
     'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'port' => getenv('DB_PORT')
 ];
 
